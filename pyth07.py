253f28633ff401f3d818a4bc2b2b9dbad6b8b0d9
mahesh m <m.basthi.7396@gmail.com>
2023-08-17 12:45:23 -0500

    change7

---
+++ pyth07.py
@@ -0,0 +1,8 @@
+import numpy as np
+a=np.array([[2,3,4],[1,6,5]])
+b=np.array([[2,5,6],[6,7,4]])
+print("after adding two metrisies :",a+b)
+print("after multiplying two metrisies :",a*b)
+print("after subtraction two metrisies :",a-b)
+
+
