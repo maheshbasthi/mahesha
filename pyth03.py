26e6faf1ba944653b41b005a656ecbeb1a453167
mahesh m <m.basthi.7396@gmail.com>
2023-08-17 12:22:35 -0500

    change3

---
+++ pyth03.py
@@ -0,0 +1,13 @@
+import numpy as np
+a=np.array([[1,2,3]])
+print("after addindin 1 is :\n",a+1)
+
+print("after addindin 2 is :\n",a+2)
+
+print("after multiplying is :\n",a*2)
+
+print("after multyplying a^2 is :\n",a**2)
+
+print("after subtraction is :\n",a-1)
+
+print("after multyplying a^3 is :\n",a**3)
